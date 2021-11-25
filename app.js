5dfb730286fe929a6cb4009c064652a0a7534c7a
Jayson Wong <jaysonwongf@gmail.com>
2021-11-25 17:43:03 +0800

    Implemented CORS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -21,6 +22,19 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // Global middlewares
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin
+// api.zentours.com, front-end zentours.com
+// app.use(
+//   cors({
+//     origin: 'https://www.zentours.com'
+//   })
+// );
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 // Set security HTTP headers
